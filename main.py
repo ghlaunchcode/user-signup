e5fff9f47bd9e92f11f1a64dd461a676441e48bf
pskt <geoff@megaman>
2017-10-02 13:18:31 -0500

    Begin main.py

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
+from flask import Flask;
+
+g_app = Flask( '[DEBUG]' )
+
+class UserForm:
+
+    #TODO: make constructor
+    def LoadTemplate():
+        return ""
+
+    def Render():
+        strRet = LoadTemplate()
+        return strRet
+
+@g_app.route( "/" ) #no post
+def index( ):
+
+    #TODO: template argument
+    iForm = UserForm()
+
+    return iForm.Render();
+
+
+
+def main():
+    print( "begin main()")
+
+    print( "leaving main()")
+
 if __name__ == "__main__":
-    #TODO: code here
+    main()
